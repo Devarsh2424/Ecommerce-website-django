9b98ed9e866508d4c01b2117129f377a0e6e9e2e
Devarsh2424 <123289922+Devarsh2424@users.noreply.github.com>
2025-08-01 16:29:44 +0530

    django authentication completed

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -137,3 +137,16 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+from django.contrib.messages import constants as messages
+
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+}
+
+#SMTP Configuration
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'pateldevarsh718@gmail.com'
+EMAIL_HOST_PASSWORD = 'jdup pgha lask evzg'  # Use environment variable for security
+EMAIL_USE_TLS = True
